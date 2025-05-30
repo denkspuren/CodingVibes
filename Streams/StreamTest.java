c0a3c3fdc0ff593db5b597bba5c678da0418a370
denkspuren <denkspuren@users.noreply.github.com>
2025-05-30 10:17:41 +0200

    Set up testing "framework"

--- Streams/StreamTest.java
+++ Streams/StreamTest.java
@@ -1,10 +1,41 @@
+// Runs with Java 24 or higher; in Java 24 preview features must be enabled
+//
+// java --enable-preview StreamTest.java
+
+// assertTrue and assertFalse make your tests somewhat more convenient
+boolean assertBool(Boolean criteria, String intent, Object... values) {
+    if (values.length == 0) throw new IllegalArgumentException("assertBool requires at least three args");
+    int count = 0;
+    print("- " + intent + ":");
+    for (Object value : values) {
+        if (value instanceof Boolean && !value.equals(criteria))
+            throw new AssertionError(intent);
+        print(" " + ++count);
+    }
+    println(" 🆗");
+    return true; 
+}
+
+boolean assertTrue(String intent, Object... values) { return assertBool(Boolean.TRUE, intent, values); }
+boolean assertTrue(Object... values) { return assertTrue("assertTrue", values); }
+boolean assertFalse(String intent, Object... values) { return assertBool(Boolean.FALSE, intent, values); }
+boolean assertFalse(Object... values) { return assertFalse("assertFalse", values); }
+
 void main() {
-    assert SimpleStream.of("A", "B", "C").count() == 3;
-    assert Stream.of("A", "B", "C").count() == 3;
+    println("🔎 TESTING: -- Use Java 24 or higher");
 
-    assert SimpleIntStream.of(2,4,3).average() == OptionalDouble.of(3.0);
-    assert IntStream.of(2,4,3).average() == OptionalDouble.of(3.0);
+    assertTrue("Interplay of SimpleStream and SimpleIntStream via count",
+        SimpleStream.of("A", "B", "C").count() == 3,
+        Stream.of("A", "B", "C").count() == 3,
+        SimpleStream.<String>of().count() == 0,
+        Stream.<String>of().count() == 0);
 
+    assertTrue("Interplay of SimpleIntStream and SimpleStream via average",
+        SimpleIntStream.of(2,4,3).average().equals(OptionalDouble.of(3.0)),
+        IntStream.of(2,4,3).average().equals(OptionalDouble.of(3.0)),
+        SimpleIntStream.empty().average().equals(OptionalDouble.empty()),
+        IntStream.empty().average().equals(OptionalDouble.empty()));
 
+    println("✅ All tests passed!");
 }
 
